0b768303da99d62389e81d7615b7c8f23a6e895d
Zhiyuan <olivenjiang@outlook.com>
2022-03-03 17:21:50 -0500

    Update

---
+++ Week 1/hello/HelloGoodbye.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+
+    }
+
+    private String name1;
+    private String name2;
+
+    public HelloGoodbye(String name1, String name2) {
+        this.name1 = name1;
+        this.name2 = name2;
+    }
+
+    public void HelloGoobye(String name1, String name2) {
+        System.out.println("Hello " + name1 + " and " + name2 + ".");
+        System.out.println("Goodbye" + name2 + " and " + name1 + ".");
+    }
+
+}
